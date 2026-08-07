9f21d5bc879b3c7c61c513b3c8bdb3519cbf0b1f
FreakedOutGamer <74013844+ScarlettHaze@users.noreply.github.com>
2026-08-07 20:52:25 +0100

    Add ApricornJuiceFlavor4 enum
    Add enum to represent the five apricorn juice flavor types in Pokémon HeartGold/SoulSilver, mapped to their packing order within the taste value.

---
+++ PKHeX.Core/Saves/Substructures/Gen4/ApricornJuiceFlavor4.cs
@@ -0,0 +1,13 @@
+namespace PKHeX.Core;
+
+/// <summary>
+/// HG/SS Apricorn juice flavor type, in packing order within <see cref="ApricornJuice4"/>'s taste value.
+/// </summary>
+public enum ApricornJuiceFlavor4 : byte
+{
+    Hot = 0,
+    Sour = 1,
+    Astringent = 2,
+    Bitter = 3,
+    Sweet = 4,
+}
